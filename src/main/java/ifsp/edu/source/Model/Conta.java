6c09d07f0770b40c9b3176a4aeb53afbc6190366
Skip0s <61255262+SkipOs@users.noreply.github.com>
2024-08-29 18:36:25 -0300

    Update Conta.java

--- src/main/java/ifsp/edu/source/Model/Conta.java
+++ src/main/java/ifsp/edu/source/Model/Conta.java
@@ -1,76 +1,70 @@
 package ifsp.edu.source.Model;
 
 public class Conta {
-    private long id;
-    private tipoConta tipoConta;
-    private statusConta statusConta;
-    private double valor;
-    private String numeroConta;
-    private String situacao;
+  private long id;
+  private tipoConta tipoConta;
+  private statusConta statusConta;
+  private double valor;
+  private String numeroConta;
+  private String situacao;
 
-    // Construtor
-    public Conta() {
-        this.situacao = "ATIVA"; // Define a situação como ATIVA por padrão
-    }
+  public String getSituacao() {
+	return situacao;
+}
 
-    public String getSituacao() {
-        return situacao;
-    }
+public void setSituacao(String situacao) {
+	this.situacao = situacao;
+}
 
-    public void setSituacao(String situacao) {
-        this.situacao = situacao;
-    }
+public String getNumeroConta() {
+	return numeroConta;
+}
 
-    public double getValor() {
-        return numeroConta;
-    }
+public void setNumeroConta(String numeroConta) {
+	this.numeroConta = numeroConta;
+}
 
-    public void setValor(String valor) {
-        this.valor = valor;
-    }
+public long getId() {
+    return id;
+  }
 
-    
-    public String getNumeroConta() {
-        return numeroConta;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public void setNumeroConta(String numeroConta) {
-        this.numeroConta = numeroConta;
-    }
+  public tipoConta getTipoConta() {
+    return tipoConta;
+  }
 
-    public long getId() {
-        return id;
-    }
+  public void setTipoConta(tipoConta tipoConta) {
+    this.tipoConta = tipoConta;
+  }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+  public statusConta getStatusConta() {
+    return statusConta;
+  }
 
-    public tipoConta getTipoConta() {
-        return tipoConta;
-    }
+  public void setStatusConta(statusConta statusConta) {
+    this.statusConta = statusConta;
+  }
 
-    public void setTipoConta(tipoConta tipoConta) {
-        this.tipoConta = tipoConta;
-    }
+  public double getValor() {
+    return valor;
+  }
 
-    public statusConta getStatusConta() {
-        return statusConta;
-    }
+  public void setValor(double valor) {
+    this.valor = valor;
+  }
 
-    public void setStatusConta(statusConta statusConta) {
-        this.statusConta = statusConta;
-    }
+  // Enums for account type and status
+  public enum tipoConta {
+    CORRENTE,
+    POUPANCA
+  }
 
-    // Enums for account type and status
-    public enum tipoConta {
-        CORRENTE,
-        POUPANCA
-    }
-
-    public enum statusConta {
-        OURO,
-        PRATA,
-        BRONZE
-    }
+  public enum statusConta {
+    OURO,
+    PRATA,
+    BRONZE
+  }
 }
