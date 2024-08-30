d0d5f8224d800f63f7b1b8bdf94514a9a4de234b
Skip0s <61255262+SkipOs@users.noreply.github.com>
2024-08-30 11:12:31 -0300

    Update DaoConta.java

--- src/main/java/ifsp/edu/source/DAO/DaoConta.java
+++ src/main/java/ifsp/edu/source/DAO/DaoConta.java
@@ -83,6 +83,7 @@ public class DaoConta {
 	            p.setNumeroConta(rs.getString("numero_conta"));
 	            p.setTipoConta(tipoConta.valueOf(rs.getString("tipo")));  // Se `tipoConta` for um enum
 	            p.setStatusConta(statusConta.valueOf(rs.getString("status")));
+	            p.setSituacao(sitaucao.valueOf(rs.getString("situacao")));
 	            p.setValor(rs.getDouble("valor"));
 	        }
 	    } catch (Exception e) {
