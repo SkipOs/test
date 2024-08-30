a7c7e8014611336c7e6b7fa90ca06b265fa20dac
Skip0s <61255262+SkipOs@users.noreply.github.com>
2024-08-30 11:43:22 -0300

    Create PixReques.java

---
+++ src/main/java/ifsp/edu/source/Request/PixReques.java
@@ -0,0 +1,40 @@
+package ifsp.edu.source.Request;
+
+public class TransferenciaRequest {
+	    private String idContaRemetente; // og long
+	    private String numeroContaDestinatario;
+	    private double valorTransferencia;
+	    private String senha;
+
+	    // Getters e Setters
+	    public String getIdContaRemetente() {// og long
+	        return idContaRemetente;
+	    }
+
+	    public void setIdContaRemetente(String idContaRemetente) {// og long
+	        this.idContaRemetente = idContaRemetente;
+	    }
+
+	    public String getNumeroContaDestinatario() {
+	        return numeroContaDestinatario;
+	    }
+
+	    public void setNumeroContaDestinatario(String numeroContaDestinatario) {
+	        this.numeroContaDestinatario = numeroContaDestinatario;
+	    }
+
+	    public double getValorTransferencia() {
+	        return valorTransferencia;
+	    }
+
+	    public void setValorTransferencia(double valorTransferencia) {
+	        this.valorTransferencia = valorTransferencia;
+	    }
+	    
+		public String getSenha() {
+			return senha;
+		}
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+}
